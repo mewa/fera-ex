21367fa79dd0120450e52af2c0bcdd1324201b22
mewa <marcin.k.chmiel@gmail.com>
2024-05-22 21:40:06 +0200

    Add rate limiter

--- lib/fera.ex
+++ lib/fera.ex
@@ -3,10 +3,39 @@ defmodule Fera do
   Helper module for initializing Fera client
   """
 
+  defmodule RateLimiter do
+    @behaviour Tesla.Middleware
+
+    @impl true
+    def call(req, next, options) do
+      with {:ok, resp} <- Tesla.run(req, next) do
+        retry_after = Tesla.get_header(resp, "retry-after")
+  
+        if retry_after do
+            retry_after = Integer.parse(retry_after) |> elem(0)
+
+          Process.send_after(self(), :try_again, retry_after * 1000)
+
+          receive do
+            :try_again ->
+                call(req, next, options)
+          end
+        else
+          {:ok, resp}
+        end
+
+      end
+    end
+
+  end
+
   def new(secret_key) do
     Fera.Connection.new()
     |> Map.update!(:pre, fn middleware ->
-      middleware ++ [{Tesla.Middleware.Headers, :call, [[{"secret-key", secret_key}]]}]
+      middleware ++ [
+        {Tesla.Middleware.Headers, :call, [[{"secret-key", secret_key}]]},
+        {Fera.RateLimiter, :call, [[]]}
+      ]
     end)
   end
 end
